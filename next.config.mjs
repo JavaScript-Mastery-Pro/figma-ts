2f8631bebb5848477059065881053e97c94df14a
sujatagunale <sujata@jsmastery.pro>
2024-01-29 08:05:34 +0530

    webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+      canvas: "commonjs canvas",
+    });
+    // config.infrastructureLogging = { debug: /PackFileCache/ };
+    return config;
+  },
   images: {
     remotePatterns: [
       {
